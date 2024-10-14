c5717a7de36b8c6557a46cb77db25de6f5bc3838
rizalarfani <arfanirizal22@gmail.com>
2024-10-14 16:48:27 +0700

    first commit

---
+++ Fundamental/src/main/kotlin/Variable.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // Dengan mengunakan var
+    var company: String = "Poltek Harber"
+    company = "Politeknik Harapan Bersama Tegal"
+    println(company)
+
+    // Dengan mengunakan val
+    val companyAddress = "Jl. Mataram No.9, Pesurungan Lor, Kec. Margadana, Kota Tegal, Jawa Tengah"
+    val kodePos = 52147
+    //companyAddress = "Poltek Harber"
+    println(companyAddress + " " + kodePos)
+}
